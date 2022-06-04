3f5066163e79513992894f16ca14da101bb5853e
BellaBradbury <bellabradbury@yahoo.com>
2022-06-03 22:53:34 -0400

    Initial File Add
    Files added from Treehouse Download

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/*
+Treehouse Techdegree: Data Pagination and Filtering
+*/
+
+
+
+/*
+For assistance:
+   Check out the "Project Resources" section of the Instructions tab
+   Reach out in your Slack community if you have questions
+*/
+
+
+
+/*
+Create the `showPage` function
+This function will create and insert/append the elements needed to display a "page" of nine students
+*/
+
+
+
+/*
+Create the `addPagination` function
+This function will create and insert/append the elements needed for the pagination buttons
+*/
+
+
+
+// Call functions
