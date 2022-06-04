ff4a9518ee52313b91813abfdadec77dd74cc53b
BellaBradbury <bellabradbury@yahoo.com>
2022-06-04 02:42:58 -0400

    showPage function added

--- js/script.js
+++ js/script.js
@@ -2,21 +2,36 @@
 Treehouse Techdegree: Data Pagination and Filtering
 */
 
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab
-   Reach out in your Slack community if you have questions
-*/
-
-
+const studentList = document.querySelector('.student-list');
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function showPage(list, page) {
+  const startIndex = (page * 9) - 9;
+  const endIndex = page * 9;
+  studentList.innerHTML = '';
 
+  for ( let i = 0; i > list.length; i++ ) {
+    if ( i >= startIndex && i < endIndex ) {
+      let studentProfile = `
+        <li class="student-item cf">
+          <div class="student-details">
+            <img class="avatar" src="${list[i].picture.thumbnail}" alt="${list[i].name}'s Profile Picture">
+            <h3>${list[i].name.first} ${list[i].name.last}</h3>
+            <span class="email">${list[i].email}</span>
+          </div>
+          <div class="joined-details">
+            <span class="date">Joined ${list[i].registered.date}</span>
+          </div>
+        </li>
+      `;
+
+      studentList.insertAdjacentHTML('beforeend', studentProfile);
+    }
+  }
+}
 
 
 /*
